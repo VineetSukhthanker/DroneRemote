42359efe9611dd828a510e606abccd110b28e700
Vineet Sukhthanker <vineetsuk@gmail.com>
2022-05-16 17:23:43 +0530

    throttle control/arm working

--- src/App.js
+++ src/App.js
@@ -1,24 +1,77 @@
 import "./App.css";
 import * as React from "react";
-import { FormControlLabel, FormGroup, Switch, Slider } from "@mui/material";
+import axios from "axios";
+import {
+  FormControlLabel,
+  FormGroup,
+  Switch,
+  Slider,
+  debounce,
+} from "@mui/material";
 
 function App() {
   const [value, setValue] = React.useState(0);
   const [pitch, setPitch] = React.useState(50);
   const [yaw, setYaw] = React.useState(0);
   const [roll, setRoll] = React.useState(50);
+  const [arm, setArm] = React.useState(false);
+
+  const handleArm = (state) => {
+    if (state) {
+      axios
+        .post("http://192.168.0.108:8000/nav/arm/", { arm: true })
+        .then(() => {
+          setArm(state);
+        });
+    } else {
+      axios
+        .post("http://192.168.0.108:8000/nav/arm/", { arm: false })
+        .then(() => {
+          setArm(state);
+        });
+    }
+  };
+
+  const handleNav = (val) => {
+    let thr = 1000 + val * 10;
+    setValue(val);
+    axios
+      .post("http://192.168.0.108:8000/nav/throttle/", {
+        thr: thr,
+      })
+      .then((res) => {
+        console.log(res.data);
+      });
+  };
+
+  const delayedHandleNav = React.useCallback(
+    debounce((e) => {
+      handleNav(e);
+    }, 50),
+    []
+  );
 
   return (
     <div className="App">
       <div className="remoteContainer">
         <FormGroup>
-          <FormControlLabel control={<Switch />} label="Arm" />
+          <FormControlLabel
+            control={
+              <Switch
+                checked={arm}
+                onChange={(e) => {
+                  handleArm(e.target.checked);
+                }}
+              />
+            }
+            label="Arm"
+          />
         </FormGroup>
         Throttle
         <Slider
           value={value}
           onChange={(e) => {
-            setValue(e.target.value);
+            delayedHandleNav(e.target.value);
           }}
         />
         Pitch
